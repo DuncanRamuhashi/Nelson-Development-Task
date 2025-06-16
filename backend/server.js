3b3b08c62aa79eb7dcacd8d4075949551a21aae1
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-06-16 13:24:22 +0200

    set

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,5 @@
+require('dotenv').config(); 
+
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
@@ -7,8 +9,9 @@ app.use(require('cors')());
 
 // Welcome endpoint
 app.get('/', (req, res) => {
-    res.json('Welcome to my Api');
-  });
+  res.json('Welcome to my Api');
+});
+
 // POST endpoint
 app.post('/sort-string', (req, res) => {
   const { data } = req.body;
@@ -17,12 +20,10 @@ app.post('/sort-string', (req, res) => {
     return res.status(400).json({ error: 'Invalid input. Expected a string in "data".' });
   }
 
-  // Convert to char array, sort, and return
   const sortedArray = data.split('').sort();
   return res.json({ word: sortedArray });
 });
 
-// Start server
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
 });
