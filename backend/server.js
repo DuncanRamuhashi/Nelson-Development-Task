182bc8d6c3272eaa83582698241d3cbd3cf54b7a
Duncan Ramuhashi <duncanramohashi@gmail.com>
2025-06-16 13:12:16 +0200

    set

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,18 @@
+
 const express = require('express');
+const serverless = require('serverless-http');
+const cors = require('cors');
+
 const app = express();
-const PORT = process.env.PORT || 3000;
-
 app.use(express.json());
-app.use(require('cors')());
+app.use(cors());
 
-// Welcome endpoint
+
 app.get('/', (req, res) => {
-    res.json('Welcome to my Api');
-  });
-// POST endpoint
+  res.json('Welcome to my API');
+});
+
+
 app.post('/sort-string', (req, res) => {
   const { data } = req.body;
 
@@ -17,13 +20,9 @@ app.post('/sort-string', (req, res) => {
     return res.status(400).json({ error: 'Invalid input. Expected a string in "data".' });
   }
 
-  // Convert to char array, sort, and return
   const sortedArray = data.split('').sort();
   return res.json({ word: sortedArray });
 });
 
-// Start server
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
-module.exports = app; 
+
+module.exports = serverless(app); 
